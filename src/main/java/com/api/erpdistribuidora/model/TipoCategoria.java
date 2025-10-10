bf0c16dec8e2ee5b30349609b4af39850f4b7d0d
devvvvvs <dicasdeacademia@gmail.com>
2025-10-09 22:26:08 -0300

    update

--- src/main/java/com/api/erpdistribuidora/model/TipoCategoria.java
+++ src/main/java/com/api/erpdistribuidora/model/TipoCategoria.java
@@ -1,6 +1,5 @@
 // src/main/java/com/api/erpdistribuidora/model/TipoCategoria.java
 package com.api.erpdistribuidora.model;
 
-public enum TipoCategoria {
-    DEPARTAMENTO, CATEGORIA
-}
+public enum TipoCategoria { Departamento, Categoria }
+
