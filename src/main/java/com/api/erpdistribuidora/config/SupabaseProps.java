725eae68aa86b549ab83bb3e460bb4e46493aa07
devvvvvs <dicasdeacademia@gmail.com>
2025-10-16 23:37:22 -0300

    commit

--- src/main/java/com/api/erpdistribuidora/config/SupabaseProps.java
+++ src/main/java/com/api/erpdistribuidora/config/SupabaseProps.java
@@ -3,7 +3,6 @@ package com.api.erpdistribuidora.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "supabase")
 public class SupabaseProps {
 
